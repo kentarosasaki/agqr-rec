1ba1bce38ca477e930f5a768e95687279e6d7ce8
Kentaro Sasaki <mathomd@gmail.com>
2014-09-19 00:58:22 +0900

    mod to use subcommand

--- reclib/agqr.py
+++ reclib/agqr.py
@@ -29,6 +29,7 @@ import sys
 import uuid
 
 
+import check_schedule
 import cmd_args
 import exception
 
@@ -56,40 +57,48 @@ def option():
       default = "rtmp://fms-base1.mitene.ad.jp/agqr/aandg2",
       help = "agqr streaming url"
       "(default: rtmp://fms-base1.mitene.ad.jp/agqr/aandg2)")
-  parser.add_argument("-o", "--output", type = str,
+
+  subparsers = parser.add_subparsers()
+
+  parser_record = subparsers.add_parser("record", help = "record help")
+  parser_record.add_argument("-o", "--output", type = str,
       help = "recording mp4 file"
-      "(default: (random uuid).mp4)")
-  parser.add_argument("-l", "--length", type = int, default = 10,
+      "(default: ${installed_path}/${random_uuid}.mp4)")
+  parser_record.add_argument("-l", "--length", type = int, default = 10,
                       help = "recording time[sec](default: 10[sec])")
+  parser_record.set_defaults(func = record)
+
+  parser_timer = subparsers.add_parser("timer", help = "timer help")
+  parser_timer.add_argument("-j", "--json", type = str,
+      help = "agqr show info json file"
+      "(default: ${installed_path}/programs.json)")
+  parser_timer.set_defaults(func = timer)
+
   return parser.parse_args()
 
 
-def run(radio_path = None, json_title = None, json_length = None):
-  args = option()
+def define_path(file_path):
+  lib_path = os.path.dirname(os.path.realpath(file_path))
+  return os.path.split(lib_path)[0]
 
-  if args.debug:
-    logging.basicConfig(level=logging.DEBUG)
 
-  base_path = os.path.dirname(os.path.realpath(__file__))
+def record(args):
+# print(args.output)
+  base_path = define_path(__file__)
   logging.debug("Base binary path: %s" % base_path)
   file_uuid = uuid.uuid4()
   tmp_flv = os.path.join(base_path, ".".join([str(file_uuid), "flv"]))
   tmp_mp4 = os.path.join(base_path, ".".join([str(file_uuid), "mp4"]))
 
   # Define output mp4 file name.
-  if (args.output is None and radio_path and json_title):
-    output_mp4 = os.path.join(radio_path, ".".join([json_title, "mp4"]))
-  elif args.output is not None:
+  if args.output is not None:
     output_mp4 = args.output
   else:
     output_mp4 = tmp_mp4
   logging.debug("mp4 file name is %s" % output_mp4)
 
   # Define recording length.
-  if json_length is None:
-    length = args.length
-  else:
-    length = json_length
+  length = args.length
   logging.debug("showtime length is %s" % length)
 
   agqr_url = args.agqr_streaming_url
@@ -110,3 +119,106 @@ def run(radio_path = None, json_title = None, json_length = None):
   if os.path.exists(tmp_flv):
     # Remove existing temporary flv file if exists.
     os.remove(tmp_flv)
+
+
+def timer(args):
+  # Define json file
+  base_path = define_path(__file__)
+  logging.debug("Base binary path: %s" % base_path)
+  programs = os.path.join(base_path, "programs.json")
+  if not os.path.exists(programs):
+    raise exception.JsonFileError(programs)
+
+  target_program = check_schedule.check_target(programs)
+  if target_program is None:
+    raise exception.TimerScheduleIncorrect()
+
+  today = datetime.datetime.now().strftime("%Y%m%d")
+
+  radio_path = target_program["path"]
+  json_title = "_".join([target_program["title"], today])
+  json_length = target_program["length"]
+ #agqr.run(radio_path, json_title, json_length)
+  file_uuid = uuid.uuid4()
+  tmp_flv = os.path.join(base_path, ".".join([str(file_uuid), "flv"]))
+  tmp_mp4 = os.path.join(base_path, ".".join([str(file_uuid), "mp4"]))
+
+  # Define output mp4 file name.
+  if (radio_path and json_title):
+    output_mp4 = os.path.join(radio_path, ".".join([json_title, "mp4"]))
+  else:
+    output_mp4 = tmp_mp4
+  logging.debug("mp4 file name is %s" % output_mp4)
+
+  # Define recording length.
+  length = json_length
+  logging.debug("showtime length is %s" % length)
+
+  agqr_url = args.agqr_streaming_url
+
+  # Define rtmpdump command.
+  rtmpdump_cmd = cmd_args.record(agqr_url, length, tmp_flv)
+
+  # Define ffmpeg command.
+  ffmpeg_cmd = cmd_args.encode_to_mp4(tmp_flv, output_mp4)
+
+  # Run rtmpdump command.
+  returncode = executor(rtmpdump_cmd)
+
+  if returncode is 0:
+    # Run ffmpeg command if rtmpdump is successful.
+    executor(ffmpeg_cmd)
+
+  if os.path.exists(tmp_flv):
+    # Remove existing temporary flv file if exists.
+    os.remove(tmp_flv)
+
+
+def run(radio_path = None, json_title = None, json_length = None):
+  args = option()
+
+  if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+
+  args.func(args)
+
+#  base_path = os.path.dirname(os.path.realpath(__file__))
+#  logging.debug("Base binary path: %s" % base_path)
+#  file_uuid = uuid.uuid4()
+#  tmp_flv = os.path.join(base_path, ".".join([str(file_uuid), "flv"]))
+#  tmp_mp4 = os.path.join(base_path, ".".join([str(file_uuid), "mp4"]))
+#
+#  # Define output mp4 file name.
+#  if (args.output is None and radio_path and json_title):
+#    output_mp4 = os.path.join(radio_path, ".".join([json_title, "mp4"]))
+#  elif args.output is not None:
+#    output_mp4 = args.output
+#  else:
+#    output_mp4 = tmp_mp4
+#  logging.debug("mp4 file name is %s" % output_mp4)
+#
+#  # Define recording length.
+#  if json_length is None:
+#    length = args.length
+#  else:
+#    length = json_length
+#  logging.debug("showtime length is %s" % length)
+#
+#  agqr_url = args.agqr_streaming_url
+#
+#  # Define rtmpdump command.
+#  rtmpdump_cmd = cmd_args.record(agqr_url, length, tmp_flv)
+#
+#  # Define ffmpeg command.
+#  ffmpeg_cmd = cmd_args.encode_to_mp4(tmp_flv, output_mp4)
+#
+#  # Run rtmpdump command.
+#  returncode = executor(rtmpdump_cmd)
+#
+#  if returncode is 0:
+#    # Run ffmpeg command if rtmpdump is successful.
+#    executor(ffmpeg_cmd)
+#
+#  if os.path.exists(tmp_flv):
+#    # Remove existing temporary flv file if exists.
+#    os.remove(tmp_flv)
